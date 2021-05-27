fb9b070f08281e0dc1a992e13a24a5d816a389c7
rafaela <rafaela@LAPTOP-P87PD8I2>
2021-05-27 17:37:59 -0300

    Add project files.

---
+++ TesteDATEtIMEConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TesteDATEtIMEConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Montagem montagem = new Montagem();
+
+            Periodos periodo = new Periodos(2021, 05, 26, 10, 04, 10);
+
+            Console.WriteLine(montagem.ToExtensoDiasEHoras(periodo));
+
+            Console.ReadLine();
+        }
+    }
+}
